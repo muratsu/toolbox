146dffa1b42bc95425663961d197ff24660bf3ce
Murat Sutunc <sutunc@gmail.com>
2014-12-07 19:16:23 -0800

    windows aliases

---
+++ windows/alias.cmd
@@ -0,0 +1,8 @@
+@echo off
+
+:: Extra System Paths
+
+:: Commands
+DOSKEY s="C:\Program Files\Sublime Text 3\subl.exe" $*
+DOSKEY sb="C:\Program Files\Sublime Text 3\subl.exe" -n / 
+DOSKEY ..=cd ..
