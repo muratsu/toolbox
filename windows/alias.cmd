432b837ca8970c502e2b84f6dce9eb31d45a2a78
Murat Sutunc <muratsu@microsoft.com>
2015-01-20 17:26:48 -0800

    add more aliases

--- windows/alias.cmd
+++ windows/alias.cmd
@@ -4,8 +4,10 @@
 
 :: Commands
 DOSKEY s="C:\Program Files\Sublime Text 3\subl.exe" $*
-DOSKEY sb="C:\Program Files\Sublime Text 3\subl.exe" -n / 
+DOSKEY sb="C:\Program Files\Sublime Text 3\subl.exe" -n /
 DOSKEY ..=cd ..
+DOSKEY ...=cd ..\..
 DOSKEY c=cd c:\
 DOSKEY d=cd c:\dev
 DOSKEY g=cd c:\git
+DOSKEY gs=git status
